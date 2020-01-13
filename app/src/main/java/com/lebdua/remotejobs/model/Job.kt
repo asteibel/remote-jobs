a888d919822b84cf369e8a09fe2292cbc318cb85
Audric <audric.steibel@gmail.com>
2020-01-13 17:00:49 +0100

    added icon

--- app/src/main/java/com/lebdua/remotejobs/model/Job.kt
+++ app/src/main/java/com/lebdua/remotejobs/model/Job.kt
@@ -8,9 +8,17 @@ data class Job(
     @SerializedName("company") var company: String = "",
     @SerializedName("company_logo") var companyLogo: String = "",
     @SerializedName("tags") var tags: List<String> = arrayListOf(),
-    @SerializedName("logo") var logo: String = "",
+    @SerializedName("logo") var _logo: String = "",
     @SerializedName("description") var description: String = "",
     @SerializedName("original") var original: Boolean = false,
     @SerializedName("verified") var verified: Boolean = false,
     @SerializedName("url") var url: String = ""
-)
+) {
+
+    val logo: String
+        get() = when {
+            companyLogo.isNotBlank() -> companyLogo
+            _logo.isNotBlank() -> _logo
+            else -> ""
+        }
+}
